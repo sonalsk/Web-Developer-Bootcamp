3ccf879c524f2aebd4de5208ba29ff88cd962cac
sonalsk <sonalkushwaha039@gmail.com>
2020-06-30 17:23:44 +0530

    js mod 11

--- 11.JS-Basic-Control-Flow/Loop.js
+++ 11.JS-Basic-Control-Flow/Loop.js
@@ -1,3 +1,4 @@
+// while
 // b/w -10 and 19
 var num = -10;
 while(num <= 19){
@@ -30,4 +31,31 @@ while(d <= 50){
         console.log(d);
     }
     d++;
+}
+
+// for
+// b/w -10 and 19
+for(var num = -10; num <= 19; num++){
+    console.log(num);
+}
+
+// even b/w 10 and 40
+for(var e = 10; e <= 40; e++){
+    if(e % 2 == 0){
+        console.log(e);
+    }
+}
+
+// odd b/w 300 and 333
+for(var o  = 300; o <= 333; o++){
+    if(o % 2 != 0){
+        console.log(o);
+    }
+}
+
+// divisible by 5 and 3 b/w 5 and 50
+for(var d = 5; d <= 50; d++){
+    if((d % 5 == 0) && (d % 3 == 0)){
+        console.log(d);
+    }
 }
---
+++ 11.JS-Basic-Control-Flow/annoy.js
@@ -0,0 +1,6 @@
+var answer = prompt("Are we there yet?");
+
+while(answer.indexOf("yes") === -1){
+    var answer = prompt("Are we there yet?");
+}
+alert("Yayyy ! We made it!")
