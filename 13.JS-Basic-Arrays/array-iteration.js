f5b5e8888f98ecc16d0298cc947481cbe19ec3ff
sonalsk <sonalkushwaha039@gmail.com>
2020-08-11 17:52:40 +0530

    js arrays

--- 13.JS-Basic-Arrays/array-iteration.js
+++ 13.JS-Basic-Arrays/array-iteration.js
@@ -1,4 +1,23 @@
 colors = ["red", "yellow", "orange", "green"]
+// for loop
 for (var i = 0; i < colors.length; i += 1) {
     console.log(colors[i]);
 }
+
+// for each
+// arr.forEach(some function)
+colors.forEach(function(color){
+    // color is variable for the items in the array
+    console.log(color);
+});
+
+
+// normal function
+function printColor(color) {
+    console.log("***************")
+    console.log(color)
+    console.log("***************")
+}
+
+// for each
+colors.forEach(printColor);
--- Assignmnets/13.JS-Basic-Arrays/TODO-LIST/todo.js
+++ Assignmnets/13.JS-Basic-Arrays/TODO-LIST/todo.js
@@ -3,18 +3,46 @@ var todo = []
 
 while (answer !== "quit") {
     var answer = prompt("What would you like to do?")
+    
     if (answer === "new") {
-        var work = prompt("Enter a new todo")
-        todo.push(work);
+        addTodo();
     }
 
     else if(answer === "list") {
-        console.log(todo)
+        listTodo();
     }
 
     else if(answer == "delete") {
-        todo.pop()
+        deleteTodo();
     }
+}
+
+function addTodo() {
+    var work = prompt("Enter a new todo")
+    todo.push(work);
+    console.log(work + "added to the list!")
+}
+
+function listTodo() {
+    console.log("**********")
+    // for loop
+    for (var i = 0; i < todo.length; i++) {
+        console.log(i + ": " + todo[i]);
+    }
+
+    // for each
+    todo.forEach(todos, i) {
+        console.log(i + ": " + todos);
+    }
+    console.log("**********")
+}
+
+function deleteTodo() {
+    var index = prompt("Enter index of todo to delete")
+    if (index !== -1) {
+        odo.splice(index, 1);
+    }
+    console.log("Todo Removed")
 }
 
 console.log("OK you quit the app")
