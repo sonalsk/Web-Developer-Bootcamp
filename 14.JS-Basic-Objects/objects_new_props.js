a9e9426fbc7d4eac472f042db261c3da63502c8b
sonalsk <sonalkushwaha039@gmail.com>
2020-08-13 23:01:06 +0530

    js questions

---
+++ 14.JS-Basic-Objects/objects_new_props.js
@@ -0,0 +1,12 @@
+var person = {
+    name: "sonal",
+    age: 20,
+    city: "delhi"
+};
+
+// delete
+delete person.name;
+
+// check if key exists
+person.hasOwnProperty(name); //true
+person.hasOwnProperty(place); //flase
